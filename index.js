4d288e4d31dc4bfd95c61b971238780b78f07d64
Wewrangler <wewranglers@gmail.com>
2024-12-25 09:14:48 +0100

    little adj

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-    origin: ['http://classroom-front-pi.vercel.app/'],
+    origin: ['https://classroom-front-pi.vercel.app/'],
     //origin: ['http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
